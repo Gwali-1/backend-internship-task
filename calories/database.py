51d3d53e713fd2b32eb3de8f2283c82c3ee32da7
Gwali <gwalisam37@gmail.com>
2023-06-14 19:23:56 +0000

    added a gitignorefile, and set up database connection configuration

---
+++ calories/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+SQLITE_DATABASE_URL = "sqlite:///./records.db"
+engine = create_engine(SQLITE_DATABASE_URL)
+SESSION_FACTORY =   sessionmaker(autocommit=False, autoflush=False, bind=engine)
+base = declarative_base()
